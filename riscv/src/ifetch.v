a18c8728caf502fe73782ca564d5ff861418365a
Conless <conlesspan@outlook.com>
2023-11-19 19:14:34 +0800

    feat(src/ifetch): main part of i-fetch finished.

--- riscv/src/ifetch.v
+++ riscv/src/ifetch.v
@@ -12,24 +12,60 @@ module InstructionFetcher #(
     input wire cdb_valid,
     input wire [CDB_WIDTH-1:0] cdb_data,
 
-    // access to memory
-    input wire rd_mem_done,
-    input wire [INST_WIDTH-1:0] rd_mem_data,
-    output wire [ADDR_WIDTH-1:0] rd_mem_valid,
-    output wire [ADDR_WIDTH-1:0] rd_mem_addr
+    // to instrcution cache
+    input wire inst_cache_read_done,
+    input wire [INST_WIDTH-1:0] inst_cache_read_data,
+    output reg [ADDR_WIDTH-1:0] inst_cache_read_addr,
+
+    // to instruction decode
+    input wire inst_decode_ready,
+    output reg inst_decode_valid,
+    output reg [INST_WIDTH-1:0] inst_decode_data
   );
-  reg [ADDR_WIDTH-1:0] program_counter;
+
+  localparam IDLE = 2'b00;
+  localparam WAIT_MEM = 2'b01;
+  localparam WAIT_DECODE = 2'b10;
+  localparam STALL = 2'b11;
+
+  reg [1:0] status; // default value is 2'b00, i.e. idle
+  reg [ADDR_WIDTH-1:0] program_counter; // default value is 0, i.e. program start from 0x0
 
   always @(posedge clk) begin
     if (rst) begin
       // TODO(Conless): reset signal
     end
     else begin
-      if (cdb_valid) begin
-        // TODO(Conless): judge if there is predict result on common data bus
-      end else begin
-        // TODO(Conless): read data
-      end
+      case (status)
+        IDLE: begin
+          status <= WAIT_MEM;
+          inst_cache_read_addr <= program_counter;
+        end
+        WAIT_MEM: begin
+          if (inst_cache_read_done) begin
+            status <= WAIT_DECODE;
+            inst_decode_valid <= 1;
+            inst_decode_data <= inst_cache_read_data;
+          end
+        end
+        WAIT_DECODE: begin
+          if (inst_decode_ready) begin
+            inst_decode_valid <= 0;
+            if (false) begin
+              // TODO(Conless): branch instruction
+            end else begin
+              status <= IDLE;
+              program_counter <= program_counter + 4;
+            end
+          end
+        end
+        STALL: begin
+          if (cdb_valid) begin
+            // TODO(Conless): monitor data on common data bus
+            status <= IDLE;
+          end
+        end
+      endcase
     end
   end
 
