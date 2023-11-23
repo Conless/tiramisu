17c51386b565b3020e8bb94c91df824b916acef7
Conless <conlesspan@outlook.com>
2023-11-23 21:30:27 +0800

    feat(src/ram): using specially designed ram now.

--- riscv/src/ram.v
+++ riscv/src/ram.v
@@ -1,31 +1,31 @@
-// implements 128KB of on-board RAM
+module ram #(
+    parameter ADDR_WIDTH = 6,
+    parameter DATA_WIDTH = 8,
+    parameter RAM_WIDTH = 32
+  )
+  (
+    input wire clk,
+    input wire rst,
+    input wire rdy,
 
-module ram
-  #(
-     parameter ADDR_WIDTH = 17
-   )
-   (
-     input  wire                   clk_in,   // system clock
-     input  wire                   en_in,    // chip enable
-     input  wire                   r_nw_in,  // read/write select (read: 1, write: 0)
-     input  wire  [ADDR_WIDTH-1:0] a_in,     // memory address
-     input  wire  [ 7:0]           d_in,     // data input
-     output wire  [ 7:0]           d_out     // data output
-   );
+    input wire [ADDR_WIDTH-1:0] addr_a,
+    output reg [RAM_WIDTH-1:0] dout_a,
 
-  wire       ram_bram_we;
-  wire [7:0] ram_bram_dout;
+    input wire [ADDR_WIDTH-1:0] addr_b,
+    input wire [RAM_WIDTH-1:0] din_b,
+    input wire we_b,
+    output reg [RAM_WIDTH-1:0] dout_b
+  );
 
-  single_port_ram_sync #(.ADDR_WIDTH(ADDR_WIDTH),
-                         .DATA_WIDTH(8)) ram_bram(
-                         .clk(clk_in),
-                         .we(ram_bram_we),
-                         .addr_a(a_in),
-                         .din_a(d_in),
-                         .dout_a(ram_bram_dout)
-                       );
+  reg [DATA_WIDTH-1:0] ram [2**ADDR_WIDTH-1:0];
+  reg [ADDR_WIDTH-1:0] q_addr_a;
+  reg [ADDR_WIDTH-1:0] q_addr_b;
 
-  assign ram_bram_we = (en_in) ? ~r_nw_in      : 1'b0;
-  assign d_out       = (en_in) ? ram_bram_dout : 8'h00;
+  always @(posedge clk) begin
+    if (we)
+      ram[addr_a] <= din_a;
+    dout_a <= ram[q_addr_a];
+    dout_b <= ram[q_addr_b];
+  end
 
 endmodule
